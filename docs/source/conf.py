4cced8b07ee88b5d1e43340b4a2a58b61f59896e
Daniele Massaro <d.massaro.26@gmail.com>
2023-05-11 00:27:08 +0200

    Add sphinx documentation config

---
+++ docs/source/conf.py
@@ -0,0 +1,64 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os.path
+from datetime import datetime
+import re
+
+project = 'SingletScalar-DM'
+copyright = f'{datetime.now().year}, Mattia Di Mauro'
+author = 'Mattia Di Mauro'
+
+package_root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
+# load version number from setup.cfg
+with open(os.path.join(package_root_path, 'setup.cfg'), 'r') as f:
+    fstring = str(f.read())
+    version = re.search(r'version\s*=\s*(?P<version>\d+\.\d+\.\d+)', fstring).group(1)
+
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
+    'sphinx_codeautolink',
+    'sphinx_gallery.gen_gallery'
+]
+
+# Napoleon settings
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+
+# Sphinx Gallery
+sphinx_gallery_conf = {
+    'examples_dirs': os.path.join(package_root_path, 'examples'),
+    'gallery_dirs': 'examples_gallery',
+    'download_all_examples': False,
+    'remove_config_comments': True,
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. SingletScalar-DM documentation master file, created by
+   sphinx-quickstart on Thu May 11 00:07:44 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SingletScalar-DM's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
